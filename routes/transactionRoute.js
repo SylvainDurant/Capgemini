5888c712cdf391123228fdeeba54720b598339a4
Sylvain <sy.durant@hotmail.com>
2021-08-10 22:58:51 +0200

    added initial transaction in transaction's historic

--- routes/transactionRoute.js
+++ routes/transactionRoute.js
@@ -1,17 +1,31 @@
 const express = require('express');
 const router = express.Router();
 const CurrentAccount = require("../models/currentAccount");
+const Transaction = require('../models/transaction');
 
-router.put('/newTransaction', (req, res) => {
+router.put('/initialCreditTransaction', (req, res) => {
     const {customerID,initialCredit} = req.body;
+
+    // prepare the transaction
+    let transaction = Transaction({
+        value: initialCredit
+    })
 
     // find the account and make the transaction
     try { CurrentAccount.findOneAndUpdate({"userInformations": customerID}, {
             credit: initialCredit,
+            $push: {Transactions: transaction._id}
         }, (error) => {
             if (error) { 
                 res.send(error);
             }else{
+                // save the transaction
+                transaction.save((error) => {
+                    if (error) {
+                        return res.send(error);
+                    }
+                })
+
                 res.sendStatus(200);
             }
         }
