487b007c0e59163d42a2d09fe5edd9bad359c15c
Sylvain <sy.durant@hotmail.com>
2021-08-13 14:01:29 +0200

    sending back account number rather than account id

--- routes/currentAccountRoute.js
+++ routes/currentAccountRoute.js
@@ -22,65 +22,60 @@ router.get('/userInformations/:accountNumber', (req, res) => {
 router.post('/newCurrentAccount', (req, res) => {
     const {customerID,initialCredit} = req.body;
 
-    // check if the customerID given is valid
-    if (customerID.match(/^[0-9a-fA-F]{24}$/)) {
-        // find the already existing customer
-        try { Customers.findById(customerID).exec((error, customer) => {
-            if (error) { return res.send(error); }
+    // find the already existing customer
+    try { Customers.findById(customerID).exec((error, customer) => {
+        if (error) { return res.send(error); }
 
-            if (!customer) {
-                return res.send({error:"There is no existing customer with this id."});
-            } else {
-                // check if customer already have a account
-                CurrentAccount.find({userInformations: customer._id}).then( async (account) => {
-                    if (account.length != 0) { 
-                        res.send("This customer already has a account: "+ account.accountNumber)
-                    } else {
-                        // generate account's number
-                        let accountNumber = await generateAccountNumber()
+        if (!customer) {
+            return res.send({error:"There is no existing customer with this id."});
+        } else {
+            // check if customer already have a account
+            CurrentAccount.find({userInformations: customer._id}).then( async (account) => {
+                if (account.length != 0) { 
+                    res.send("This customer already has an account: "+ account.accountNumber)
+                } else {
+                    // generate account's number
+                    let accountNumber = await generateAccountNumber()
 
-                        // asign the customer to an account
-                        let new_account = new CurrentAccount({
-                            userInformations: customer._id,
-                            accountNumber: accountNumber
-                        });
+                    // asign the customer to an account
+                    let new_account = new CurrentAccount({
+                        userInformations: customer._id,
+                        accountNumber: accountNumber
+                    });
 
-                        // save the account in the db
-                        new_account.save((error) => {
-                            if (error) {
-                                return res.send(error);
+                    // save the account in the db
+                    new_account.save((error) => {
+                        if (error) {
+                            return res.send(error);
+                        }
+                    })
+
+                    // transaction
+                    if (initialCredit > 0) {
+                        // call the api's endpoint for transactions
+                        await axios.put(`http://${req.headers.host}/api/transaction/newTransaction`,{
+                            "sender": "initial",
+                            "receiver": new_account.accountNumber,
+                            "transactionValue": initialCredit 
+                        })
+                        .then((response) => {
+                            if (response.status === 200) {
+                                res.status(200).send(new_account.accountNumber);
+                            } else {
+                                res.send("Transaction error: " + response.status);
                             }
                         })
-
-                        // transaction
-                        if (initialCredit > 0) {
-                            // call the api's endpoint for transactions
-                            await axios.put(`http://${req.headers.host}/api/transaction/newTransaction`,{
-                                "sender": "initial",
-                                "receiver": new_account.accountNumber,
-                                "transactionValue": initialCredit 
-                            })
-                            .then((response) => {
-                                if (response.status === 200) {
-                                    res.status(200).send(new_account._id);
-                                } else {
-                                    res.send("Transaction error: " + response.status);
-                                }
-                            })
-                            .catch((error) => {
-                                res.send(error);
-                            });
-                        } else {
-                            res.status(200).send(new_account._id);
-                        }
+                        .catch((error) => {
+                            res.send(error);
+                        });
+                    } else {
+                        res.status(200).send(new_account.accountNumber);
                     }
-                })
-            }
-        })} catch (error) {
-            res.send(error);
+                }
+            })
         }
-    } else {
-        return res.send({error:"This id is not valid."});
+    })} catch (error) {
+        res.send(error);
     }
 });
 
