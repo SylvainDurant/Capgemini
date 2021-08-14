c332d8cb072b57df3efbd1a8402b529f98e3f7db
Sylvain <sy.durant@hotmail.com>
2021-08-14 22:20:37 +0200

    fixed transaction occuring even with inssuficent funds

--- routes/transactionRoute.js
+++ routes/transactionRoute.js
@@ -8,49 +8,57 @@ router.put('/newTransaction', (req, res) => {
 
     if (transactionValue < 0) {return res.send({error:"Transaction value must be greater than 0"});}
 
-    // check if sufficient funds
-    CurrentAccount.findOne({"accountNumber": sender}).then((senderAccount) => {
-        if (!senderAccount) {
-            return res.send({error:"This account does not exist."})
-        } else if (senderAccount.credit < transactionValue) {
-            return res.send({error:"insufficient funds"})
-        }
-    });
+    const makeTransaction = () => {
+        // prepare the transaction
+        let transaction = new Transaction({
+            value: transactionValue,
+            sender: sender, 
+            receiver: receiver
+        })
 
-    // prepare the transaction
-    let transaction = new Transaction({
-        value: transactionValue,
-        sender: sender, 
-        receiver: receiver
-    })
+        // find receiver transaction
+        CurrentAccount.findOne({"accountNumber": receiver}).then( async (receiverAccount) => {
+            if (!receiverAccount) {
+                return res.send({error:"This account does not exist."})
+            } else {
+                if (sender != "initial") {
+                    let valueToSend = transactionValue * -1;
 
-    // find receiver transaction
-    CurrentAccount.findOne({"accountNumber": receiver}).then( async (receiverAccount) => {
-        if (!receiverAccount) {
-            return res.send({error:"This account does not exist."})
-        } else {
-            if (sender != "initial") {
-                let valueToSend = transactionValue * -1;
+                    // make the transaction on the sender account
+                    await CurrentAccount.findOneAndUpdate({"accountNumber": sender}, {
+                        $inc : {'credit' : (valueToSend)},
+                        $addToSet: {transactions: transaction._id}
+                    })
+                }
 
-                // make the transaction on the sender account
-                await CurrentAccount.findOneAndUpdate({"accountNumber": sender}, {
-                    $inc : {'credit' : (valueToSend)},
+                // make the transaction to the receiver account
+                await CurrentAccount.findOneAndUpdate({"accountNumber": receiver}, {
+                    $inc : {'credit' : transactionValue},
                     $addToSet: {transactions: transaction._id}
                 })
+
+                // save the transaction
+                transaction.save();
+
+                res.send(transaction);
             }
-
-            // make the transaction to the receiver account
-            await CurrentAccount.findOneAndUpdate({"accountNumber": receiver}, {
-                $inc : {'credit' : transactionValue},
-                $addToSet: {transactions: transaction._id}
-            })
-
-            // save the transaction
-            transaction.save();
-
-            res.send(transaction);
-        }
-    })
+        })
+    }
+    
+    // check if sufficient funds
+    if (sender != "initial"){
+        CurrentAccount.findOne({"accountNumber": sender}).then((senderAccount) => {
+            if (!senderAccount) {
+                return res.send({error:"This account does not exist."})
+            } else if (senderAccount.credit < transactionValue) {
+                return res.send({error:"insufficient funds"})
+            } else {
+                makeTransaction();
+            }
+        });
+    } else {
+        makeTransaction();
+    }
 });
 
 module.exports = router;
